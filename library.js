d4bee6af538fbfd867442e475d8c54b600ea285f
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-03-25 19:55:03 +0200

    added the html boilerplate, css styles and event listener on btn to show the form

---
+++ library.js
@@ -0,0 +1,6 @@
+let showForm = document.querySelector(".addBook");
+let form = document.querySelector(".form");
+
+showForm.addEventListener("click", function(){
+    form.classList.toggle("active")
+})
